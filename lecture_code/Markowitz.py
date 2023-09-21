3970a3db270c376c4fa3333dd4452fb0618341e5
김주환 <asas4539@hanyang.ac.kr>
2023-09-21 21:49:44 +0900

    .

---
+++ lecture_code/Markowitz.py
@@ -0,0 +1,83 @@
+import os
+import numpy as np
+import pandas as pd
+from cvxopt import matrix
+from cvxopt.solvers import qp
+import matplotlib.pyplot as plt
+
+
+class Optimization:
+    def __init__(self, data: pd.DataFrame, benchmark: pd.DataFrame):
+        data['Date'] = pd.to_datetime(data['Date'])
+        data.set_index('Date', inplace=True)
+        self.data = data.iloc[:, :4]
+
+        present_months = self.data.iloc[1:, :]
+        past_months = self.data.iloc[:-1, :]
+        past_months.index = present_months.index
+        self.mom_data = (present_months - past_months) / past_months
+
+        self.benchmark = benchmark
+        self.cov = []
+        self.mean = []
+        self.sol = []
+        self.profit = []
+
+    def assume_best_cov(self):
+        Cov = matrix(self.mom_data.cov().values)
+        self.cov = Cov
+
+    def assume_best_mean(self):
+        Mean = matrix(self.mom_data.mean())
+        self.mean = Mean
+
+    def optimalize_portfolio(self, Cov, Mean):
+        n = 4
+        r_min = 0.035
+
+        G = matrix(np.concatenate((-np.transpose(Mean), -np.identity(n)), 0))
+        h = matrix(np.concatenate((-np.ones((1, 1)) * r_min, np.zeros((n, 1))), 0))
+
+        A = matrix(1.0, (1, n))
+        b = matrix(1.0)
+        q = matrix(np.zeros((n, 1)))
+        sol = qp(Cov, q, G, h, A, b)
+
+        self.sol = sol['x']
+
+    def calculate_return(self, Plot: bool):
+        sol = pd.DataFrame(self.sol).T
+        profit_df = self.mom_data * sol.values
+
+        profit_df = profit_df + 1
+        profit_df = np.log(profit_df)
+
+        profit_df['Sum'] = profit_df.sum(axis=1)
+        profit = profit_df['Sum']
+        self.profit = profit
+
+        if Plot:
+            result = profit.cumsum(axis=0)
+            plt.figure(figsize=(10, 6))
+            plt.plot(result)
+            plt.title('return')
+            plt.xlabel('Date')
+            plt.ylabel('Value')
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            plt.show()
+
+
+if __name__ == "__main__":
+    input_dir = '../lecture_data'
+    file = 'dataset.csv'
+    df = pd.read_csv(os.path.join(input_dir, file))
+    benchmark = pd.concat([df['Date'], df['KOSPI'], df['KOR10Y']], axis=1)
+    df.drop(columns=['KOSPI', 'KOR10Y'], inplace=True)
+
+    optimal = Optimization(df, benchmark)
+    optimal.assume_best_cov()
+    optimal.assume_best_mean()
+    optimal.optimalize_portfolio(optimal.cov, optimal.mean)
+    optimal.calculate_return(Plot=False)
+    optimal.profit.to_csv(os.path.join(input_dir, 'profit.csv'))
---
+++ lecture_code/Scratch.py
@@ -0,0 +1,18 @@
+import warnings
+import Markowitz as Op
+from Markowitz import *
+
+warnings.filterwarnings("ignore")
+
+if True:
+    input_dir = "../lecture_data"
+    file = "dataset.csv"
+    df = pd.read_csv(os.path.join(input_dir, file))
+    benchmark = pd.concat([df['Date'], df['KOSPI'], df['KOR10Y']], axis=1)
+    df.drop(columns=['KOSPI', 'KOR10Y'], inplace=True)
+
+optimal = Op.Optimization(df, benchmark)
+optimal.assume_best_cov()
+optimal.assume_best_mean()
+optimal.optimalize_portfolio(optimal.cov, optimal.mean)
+optimal.calculate_return(Plot=True)
